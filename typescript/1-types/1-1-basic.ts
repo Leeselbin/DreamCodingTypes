ecaa6b5e526f7fc8a668361c108ea417d7007691
Binsanity95 <84565641+Leeselbin@users.noreply.github.com>
2022-10-11 11:05:20 +0900

    1-1

--- typescript/1-types/1-1-basic.ts
+++ typescript/1-types/1-1-basic.ts
@@ -19,9 +19,40 @@
   let age: number | undefined;
   age = undefined;
   age = 1;
+  function find(): number | undefined {
+    return undefined;
+  }
 
   //null
   let person: string | null;
   person = null;
   person = "test";
+
+  // unknown 안쓰는거 권장
+  let notSure: unknown = 0;
+  notSure = "he";
+  notSure = true;
+
+  // any 안쓰는거 권장
+  let anything: any = 0;
+  anything = "hello";
+
+  // void - 아무것도 리턴하지 않는경우 생략가능
+  function print(): void {
+    console.log("hello");
+    return;
+  }
+  let unusable: void = undefined; // 절대 x
+
+  // never - return 을 던질수없다.
+  function throwError(message: string): never {
+    //message -> server (log)
+    throw new Error(message);
+  }
+
+  // object - 쓰지 않는것이 좋다
+  let obj: object;
+  function acceptSomeObject(obj: object) {}
+  acceptSomeObject({ name: "ellie" });
+  acceptSomeObject({ animal: "dog" });
 }
