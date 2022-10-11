7280d4075e24a4fb62242d0bb4d5fe9146bf56a5
Binsanity95 <84565641+Leeselbin@users.noreply.github.com>
2022-10-11 10:00:51 +0900

    1-1

---
+++ typescript/1-types/1-1-basic.ts
@@ -0,0 +1,27 @@
+{
+  /**
+   * JavaScirpt
+   * Primitive(원시타입 - 1가지타입만 가지는)
+   * Primitive: number, string, boolean, bigint, symbol, null, undefined
+   * Object: function, array....
+   */
+
+  //number
+  const num: number = 1;
+
+  //string
+  const str: string = "hello";
+
+  //boolean
+  const boal: boolean = true;
+
+  //undefined
+  let age: number | undefined;
+  age = undefined;
+  age = 1;
+
+  //null
+  let person: string | null;
+  person = null;
+  person = "test";
+}
--- typescript/main.js
+++
@@ -1,7 +0,0 @@
-console.log("hello world!!!!");
-var Car = /** @class */ (function () {
-    function Car() {
-        this.engine = 1;
-    }
-    return Car;
-}());
