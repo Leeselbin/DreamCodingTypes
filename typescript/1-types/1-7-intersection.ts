ef23ad17a95ea0cd529ccec7e7450ada7350a3d3
Binsanity95 <84565641+Leeselbin@users.noreply.github.com>
2022-10-18 10:43:54 +0900

    basic types study

---
+++ typescript/1-types/1-7-intersection.ts
@@ -0,0 +1,25 @@
+{
+  /**
+   * Intersection Types: &
+   */
+  type Student = {
+    name: string;
+    score: number;
+  };
+
+  type Worker = {
+    empolyeeId: number;
+    work: () => void;
+  };
+
+  function internWork(person: Student & Worker) {
+    console.log(person.name, person.empolyeeId, person.work());
+  }
+
+  internWork({
+    name: "bin",
+    score: 1,
+    empolyeeId: 123,
+    work: () => {},
+  });
+}
