f4a177721f4834f4aa4394ea37ed8d11bc2fbca2
Binsanity95 <84565641+Leeselbin@users.noreply.github.com>
2022-10-18 16:46:26 +0900

    Update 1-8-enum.ts

---
+++ typescript/1-types/1-8-enum.ts
@@ -0,0 +1,25 @@
+{
+  /**
+   * Enum
+   */
+  // JavaScript
+  const MAX_NUM = 6;
+  const MAX_STUDENTS_PER_CLASS = 10;
+  const MONDAY = 0;
+  const TUESDAY = 1;
+  const WEDNESDAY = 2;
+  const DAYS_ENUM = Object.freeze({ MONDAY: 0, TUESDAY: 1, WEDNESDAY: 2 });
+  const dayOfToday = DAYS_ENUM.MONDAY;
+
+  // TypeScript
+  enum Days {
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Satarday,
+    Sunday,
+  }
+  const day = Days.Satarday;
+}
